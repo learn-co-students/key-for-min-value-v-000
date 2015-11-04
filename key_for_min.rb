35421f38742d9075a16b1d0e8a777ad407cdc4dc
Ashley <serenehopes@gmail.com>
2015-11-04 07:16:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,17 @@
 
 def key_for_min_value(name_hash)
  min = 0
+  key = nil
+
  my_array =[]
   name_hash.each do |name,age|
     if min == 0
       min = age
+      key = name
     end
     if age <= min
-      my_array << name
+      key = name
     end
   end
-my_array.pop
+  key
 end
