b0d9448b6a5efbd35e5ab14b27db4c7c34bc3084
jakebrady5 <jakebrady5@gmail.com>
2015-11-11 21:47:53 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  current_low = Float::INFINITY
+  name_hash.each{|a, b|
+    if b < current_low
+      current_low = b
+    end
+  }
+
+  name_hash.key(current_low)
+
 end
