1e941ea17c29827664ec847162a00b0ea7bc38e8
dannyd4315 <dannyd4315@gmail.com>
2015-11-18 11:41:00 +0000

    Done

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  comparator = 1000
+  smallest = ""
+  if name_hash.empty? 
+      smallest = nil
+  else       
+    name_hash.each do | key, value |
+      if comparator > value
+      comparator = value
+      smallest = key
+      end
+    end
+  end       
+  smallest  
 end
