fb67be8b8fcaa8bde7bcb7652c2bfdc17ba5d388
lisamarie616 <lisamarie616@gmail.com>
2015-11-27 19:22:46 -0800

    add #key_for_min_value

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  values = name_hash.collect {|key,value| value}
+  swapped = true
+  while swapped
+    swapped = false
+    (values.length-1).times do |i|
+      if values[i] > values[i+1]
+        values[i], values[i+1] = values[i+1], values[i]
+        swapped = true
+      end
+    end
+  end
+  name_hash.detect do |key,value|
+    if value == values[0]
+      return key
+    end
+  end
 end
