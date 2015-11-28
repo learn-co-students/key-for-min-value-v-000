cb520f1081a97c3e7f09b430c653dfe7bb266b99
lisamarie616 <lisamarie616@gmail.com>
2015-11-27 19:30:57 -0800

    revise #key_for_min_value

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,33 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+# def key_for_min_value(name_hash)
+#   values = name_hash.collect {|key,value| value}
+#   swapped = true
+#   while swapped
+#     swapped = false
+#     (values.length-1).times do |i|
+#       if values[i] > values[i+1]
+#         values[i], values[i+1] = values[i+1], values[i]
+#         swapped = true
+#       end
+#     end
+#   end
+#   name_hash.detect do |key,value|
+#     if value == values[0]
+#       return key
+#     end
+#   end
+# end
+
 def key_for_min_value(name_hash)
-  values = name_hash.collect {|key,value| value}
-  swapped = true
-  while swapped
-    swapped = false
-    (values.length-1).times do |i|
-      if values[i] > values[i+1]
-        values[i], values[i+1] = values[i+1], values[i]
-        swapped = true
-      end
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  name_hash.each do |key, value|
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
   end
-  name_hash.detect do |key,value|
-    if value == values[0]
-      return key
-    end
-  end
+  lowest_key
 end
