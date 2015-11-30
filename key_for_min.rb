9974c0b5970c9d2149f92704a602db9b5634da94
authorbeard <msnolan@gmail.com>
2015-11-30 12:19:38 -0500

    all tests passing

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key = nil
+  name_hash.each {| person, value| 
+    if min_key == nil
+      min_key = person
+    elsif value < name_hash[min_key]
+      min_key = person
+    end
+  } 
+  min_key
 end
+
