dee5d9107bcbfefcb7c6e927135550fc15ace392
ed-karabinus <ejkarabin@gmail.com>
2015-12-02 15:02:36 -0600

    Finished Hash Iteration Lab; added definition of #key_for_min_value to key_for_min.rb

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = nil # This is a placeholder value
+  min_value_key = nil # This is a placeholder key
+  set_min_value_flag = false
+  name_hash.each do |key, value|
+    if !set_min_value_flag
+      min_value = value
+      min_value_key = key
+      set_min_value_flag = true
+    end
+    if value < min_value
+      min_value = value
+      min_value_key = key
+    end
+  end
+  min_value_key
 end
