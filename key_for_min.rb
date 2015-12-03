c1a49296d9bc5e75ebb655ed78c837377ece8b46
ed-karabinus <ejkarabin@gmail.com>
2015-12-02 21:23:26 -0600

    Simplified definition of #key_for_min_value in key_for_min.rb

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = nil # This is a placeholder value
+  min_value = nil
   min_value_key = nil # This is a placeholder key
-  set_min_value_flag = false
   name_hash.each do |key, value|
-    if !set_min_value_flag
-      min_value = value
-      min_value_key = key
-      set_min_value_flag = true
-    end
-    if value < min_value
+    if !min_value_key || value < min_value
       min_value = value
       min_value_key = key
     end
