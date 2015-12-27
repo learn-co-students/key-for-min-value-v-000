36cdbdb106d6a00c965c166fd30c02d17e47e316
Chris Bruen <bruen.chris@gmail.com>
2015-12-26 22:04:41 -0500

    fix

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
 	collect = []
 	name_hash.each do |x,y|
 		collect[0] = x if collect[1].nil? || y < collect[1]
+		collect[1] = y if collect[1].nil? || y < collect[1]
 	end
 	collect[0]
 end
