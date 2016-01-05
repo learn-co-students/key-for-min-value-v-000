5f41307bf864213e2c3fcc6c401521c3df7845c9
fiveinfinity <jlachance1@gmail.com>
2016-01-05 14:28:42 -0600

    more elegant method solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,25 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  names = []
-  nums = []
+  lowest = nil
+  winner = nil
 
   return nil if name_hash.empty?
 
   name_hash.collect do |key, value|
-    names.push(key) #['blake', 'ashley', 'adam']
-    nums.push(value) #[500, 2, 1]
+    if lowest == nil || value < lowest
+      lowest = value 
+      winner = key
+    end
   end
-  
-  if nums[0] < (nums[1] && nums[2])
-    return names[0]
-  elsif nums[1] < (nums[0] && nums[2])
-    return names[1]
-  else
-    return names[2]
-  end
-
+  winner
 end
+
 
 
 
