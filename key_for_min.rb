511e88891ae7ab0ebf887c3b73f88f054bf793ca
fiveinfinity <jlachance1@gmail.com>
2016-01-05 13:57:12 -0600

    add #key for min value

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,26 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  names = []
+  nums = []
+
+  return nil if name_hash.empty?
+
+  name_hash.collect do |key, value|
+    names.push(key) #['blake', 'ashley', 'adam']
+    nums.push(value) #[500, 2, 1]
+  end
+  
+  if nums[0] < (nums[1] && nums[2])
+    return names[0]
+  elsif nums[1] < (nums[0] && nums[2])
+    return names[1]
+  else
+    return names[2]
+  end
 
 end
+
+
+
+
