2e24ec9ee99b2d91ea03be3751156c9f13a0d8b8
simque <simon.duncanson@gmail.com>
2016-01-16 15:56:19 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
-# Give a hash with numeric values, return the key for the smallest value
+# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
   key = nil
