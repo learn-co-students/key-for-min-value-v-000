20125e61e1b20c65fd471c76ff201586b0015907
sameera91 <sameera.bachoti@gmail.com>
2016-01-22 18:57:08 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,12 @@
 
 def key_for_min_value(name_hash)
 
+  min = name_hash[:blake]
+  name_hash.each {|key,value|
+    if value < min
+      min = value
+    end
+  }
+
+  return name_hash.index(min)
 end
