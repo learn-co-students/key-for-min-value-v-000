2acc64f1e5a532e35884ed17e2ad4b324abb93dd
bedwardsphoto <brian@brianedwardsphotography.com>
2016-02-12 09:34:23 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.find do|name,value|
+    if name_hash.all? {|name2, value2| value2>=value}
+      return name
+    end
+  end
 end
