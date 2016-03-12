6462d7fbc6b72abc32713912533f0ff8c85d35b5
jchu4483 <jchu4483@gmail.com>
2016-03-12 17:46:44 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash.each_key {|key1, key2| key1 <=> key2}
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value #lowest_value = nil in this part
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
+
+=begin
+  
+The key cencept here is comparing each value to "nil". By setting nil
+as the deault comparision value - we've set a benchmark to test each iteration.
+So, during each iteration, if the lowest value equals to nil OR ANY value in the hash is less than nil
+then the new lowest value is stored in lowest_value (Line 9).
+Also, lowest_key will then be reset to the lowest key(Line 10).
+
+=end
