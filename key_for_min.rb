381068c0e51913b8b678bb0cbddcfa344a3db650
yeungn <nin.yeung@gmail.com>
2016-04-28 13:08:39 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,15 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash.empty?
-  # Convert hash to array
-  name_a = name_hash.to_a
 
-  # Default key value
-  d_value= 1000
-  d_key= 0
+  lowest_value = 1000
+  lowest_key  = nil
 
-  # Iterate new array
-  name_a.each do |i|
-      # If current value is lower than default, change value&key
-      if i[1] < d_value
-        d_value = i[1]
-        d_key = i[0]
-      end
-   end
-  return d_key
+  name_hash.each do | key, value |
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
