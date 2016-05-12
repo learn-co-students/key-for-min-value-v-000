9f1923539a4d7967d11c8fddf9b51ff9f26d28fb
mjdele <mike.delehant@gmail.com>
2016-05-11 21:21:36 -0400

    included more efficient solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -25,3 +25,19 @@ def key_for_min_value(name_hash)
     min_key
   end
 end
+
+# or.....
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
+
+
