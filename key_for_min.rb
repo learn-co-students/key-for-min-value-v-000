6f4b77de8759867ff775ee3c121ac7d79d0f7caf
vicision <duncan.j.mclachlan@gmail.com>
2016-05-27 16:57:45 -0400

    Done: all tests passing

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  min_key = nil
+  min_val = nil
+  name_hash.each do |key, value|
+    if min_val == nil || value < min_val
+      min_val = value
+      min_key = key
+    end
+  end
+  min_key
 end
