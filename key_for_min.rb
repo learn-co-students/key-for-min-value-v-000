63520402d3ff86170effb42cec9eab77bf281280
Lenn Hypolite <lenntriniman@hotmail.com>
2016-05-29 14:58:01 -0400

    initial commit, passed all tests, added comments

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+	#set initial placeholder variables to compare to each value everytime you itereate
+	min_value = false
+	min_key = false
+	if name_hash.empty? #check outside the loop so that the code in the loop runs
+			 return nil
+	end
+  name_hash.each do | key, value | #start loop
+			if min_value == false  #check to see if it's false first
+			   min_value = value  #min_value is now equal to false
+			   min_key = key
+				 # we don't want min_value to stay false so we reset it below
+		  elsif min_value > value  # reset and assign the current value to the min value to compare and assign  the lowest value
+			 	 min_value = value
+			   min_key = key
+  	 	end
+	end
+ return	min_key
 end
