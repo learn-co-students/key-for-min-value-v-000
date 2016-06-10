12c166c6023bec2ad0958aee37fedd0dbf0975e7
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 21:29:34 +0000

    passed all tests. may want to refactor

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    names = []
+    values = []
+    name_hash.each do |name, value|
+       if values.empty?
+           values << value
+           names << name
+       elsif value < values.last
+            values << value 
+            names << name
+        end    
+    end
+    names.last
 end
