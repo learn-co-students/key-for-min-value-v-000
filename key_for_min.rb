89df7e423614c5d8b5e769ee788c38abbac26a75
aarondevon <me@aarondevon.com>
2016-06-15 15:01:43 -0500

    sort to find smallest value

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# Can't use the following methods: #keys, #values, #min , #sort, #min_by
 
 def key_for_min_value(name_hash)
-
+  value_array = []
+  name_hash.each do |key, value|
+    value_array << value
+  end
+  control_value = value_array[0]
+  value_array.each do |number|
+    if control_value >= number
+      control_value = number
+    end
+  end
+  name_hash.each do |key, value|
+    if value == control_value
+      return key
+    end
+  end
+  return nil
 end
