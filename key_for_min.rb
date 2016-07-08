73de6ee8212bb6c8ffeb3e2698173c598b9d6dfe
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-08 20:23:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  comparative = nil
+  name_hash.each do |key, value|
+    if comparative == nil || comparative == 0
+      comparative = value
+    elsif comparative > value
+      comparative = value
+    end
+  end
 
+  return_key = nil
+  name_hash.each do |key, value|
+    if comparative == value
+      return_key = key
+    end
+  end
+  return_key
 end
