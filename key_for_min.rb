12dc642b869af8b92a3ed7fe6ca237e77ee77895
Lani1321 <lani.berry13@gmail.com>
2016-07-11 19:06:00 -0400

    add .each to return key with the smallest value

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  lowest_variable = 99999999999
+  lowest_key = nil
+  name_hash.each do |k, v|
+    if v < lowest_variable
+      lowest_variable = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
