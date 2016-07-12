47a7cfd82134b9517bbb5f5100bdf1fc78aa99df
Neil Ryan <neilmarkoliverryan@gmail.com>
2016-07-12 03:43:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-  	min_value = 100
+  	min_value = Float::INFINITY
   	name_hash.each do |key, value|
   		if name_hash[key] < min_value
   			min_value = name_hash[key]
