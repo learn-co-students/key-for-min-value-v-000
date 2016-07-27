705724bbe174754eabb39236d47bbe0050eb53d5
William Jones <enquiries@forestfed.com>
2016-07-27 21:19:28 +0000

    collect data from hash

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  minimum_value = 999999
+  save_key = ""
+  if name_hash == {}
+    return nil
+  else
+    name_hash.each do |key, value|
+      if value < minimum_value
+        minimum_value = value
+        save_key = key
+      end
+    end
+  end
+  save_key
 end
