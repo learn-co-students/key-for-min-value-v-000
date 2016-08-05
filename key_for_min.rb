80b273847cdb69b712624201fd55f1934569b183
John Schlesinger <jschles1@ramapo.edu>
2016-08-05 00:19:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value = 999999 # Sets default values #
+  min_key = ""
+  if name_hash == {} # An empty hash will return nil #
+    return nil
+  else
+    name_hash.each do |key, value| # If value being iterated is < min_value, then that value will become the new min_value, and the associated key will become min_key #
+      if value < min_value
+        min_value = value
+        min_key = key
+      end
+    end
+  end
+  min_key
 end
