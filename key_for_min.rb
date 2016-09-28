31a22eafdbc1b3bbea70c3e9390d2deb0133ef55
Julie Anne McManus <julieannemcmanus@Julies-MacBook-Pro.local>
2016-09-28 09:59:29 -0400

    completed key_for_min_value assignment

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  min_key = nil
+  min_value = nil
+  hash.each do |k, v|
+    if min_value == nil || v < min_value
+      min_value = v
+      min_key = k
+    end
+  end
+  min_key
 end
