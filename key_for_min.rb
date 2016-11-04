b1472cc07afba3104fb0b600854279cb4f226951
derek maloney <derekjmaloney@gmail.com>
2016-11-04 17:14:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  height_array = name_hash.collect {|name, height| height}
+  height_array.each do |inches|
+    if inches <= height_array[0] && inches < height_array[1]
+      height_array[0] = inches
+    elsif inches >= height_array[0] && inches < height_array[1]
+      height_array[1] = inches
+    end
+  end
+  name_hash.key(height_array[0])
 end
