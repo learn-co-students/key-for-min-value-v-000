542437a33554645fc62d004c40f1fbd09716f31c
Sai Petluri <spetluri.1@gmail.com>
2016-12-04 00:23:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,19 +4,10 @@
 def key_for_min_value(name_hash)
   minAge = nil
   minName = nil
-  if name_hash == nil
-    minName
-  else
-    name_hash.each do |name,age|
-      if minAge == nil
-        minName = name
-        minAge = age
-      else
-        if age < minAge
-          minAge = age
-          minName = name
-        end
-      end
+  name_hash.each do |k,v|
+    if minAge == nil || v < minAge
+      minName = k
+      minAge = v
     end
   end
   minName
