dfb1edc90e1fc52926031d8de8cac5ba25503f5e
charlie lee <ctlee9096@gmail.com>
2017-01-06 04:48:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,12 @@ def key_for_min_value(hash)
 lowest_key = nil
 lowest_value = nil
 hash.each do|k,v|
-  if lowest_value == nil || lowest_value < v
-    v= lowest_value
+  if lowest_value == nil || lowest_value > v
+    lowest_value = v
     #binding.pry
-    k= lowest_key
-  #elsif lowest_value < v
-  #  v = lowest_value
-  #  binding.pry
-  #s  k = lowest_key
+    lowest_key = k
   end
 end
   lowest_key
 end
+https://github.com/learn-co-curriculum/key-for-min-value/commit/5ef8507158fa6d6a1b8499b494bc9d418c8d055c
