8ec60304c40186b4dc652e76586279f6b4ddaeec
charlie lee <ctlee9096@gmail.com>
2017-01-06 04:32:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,14 @@ def key_for_min_value(hash)
 lowest_key = nil
 lowest_value = nil
 hash.each do|k,v|
-  if lowest_value == nil
+  if lowest_value == nil || lowest_value < v
     v= lowest_value
     #binding.pry
     k= lowest_key
-  else v < lowest_value
-    v = lowest_value
-    binding.pry
-    k = lowest_key
+  #elsif lowest_value < v
+  #  v = lowest_value
+  #  binding.pry
+  #s  k = lowest_key
   end
 end
   lowest_key
