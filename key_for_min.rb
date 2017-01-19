7ea52a3c92a4f569454ce9b7492d0f9c06e9ee0f
Elias Ventura <eliasventura23@gmail.com>
2017-01-19 20:22:45 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,11 @@
 def key_for_min_value(hash)
-  lowest_value = nil
   lowest_key = nil
-  hash.collect do |key, value|
-    if lowest_value == nil || value < lowest_value
+  lowest_value = Float::INFINITY
+  hash.each do |key, value|
+    if value < lowest_value
       lowest_value = value
       lowest_key = key
-
+    end
   end
-end
-lowest_key
+  lowest_key
 end
