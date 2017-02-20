46e9c6c35304747fd9bb22d5cc878ba58ef08d8b
krystlebarnes <krystlecbarnes@gmail.com>
2017-02-19 20:53:23 -0800

    add key_for_min_value method

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  max_value = 0
+  key_for_max_value = nil
+  name_hash.each do |key, value|
+    if value > max_value
+      max_value = value
+      key_for_max_value = key
+    end
+  end
+  min_value = max_value
+  key_for_min_value = key_for_max_value
+  name_hash.each do |key, value|
+    if value < min_value
+      min_value = value
+      key_for_min_value = key
+    end
+  end
+  key_for_min_value
 end
