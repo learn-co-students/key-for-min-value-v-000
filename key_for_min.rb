37ea02e3460aed60bb8cbb0c646f686b62940d66
Aimee Carney <acarney29@gmail.com>
2017-03-19 22:51:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  # Convert hash to array
+  name_a = name_hash.to_a
 
+  # Default key value
+  d_value= 1000
+  d_key= 0
+
+  # Iterate new array
+  if name_a.empty?
+  return nil
+
+    else
+    name_a.collect do |i|
+        # If current value is lower than default, change value&key
+        if i[1] < d_value
+          d_value = i[1]
+          d_key = i[0]
+        end
+     end
+     return d_key
+  end
 end
