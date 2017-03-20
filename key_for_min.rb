9ae48188b7f37dc4a1db8c4cae713c2cda3dfdb3
Sergio Mendoza <smendoza787@gmail.com>
2017-03-20 05:51:13 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,9 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   smallest_value = nil
   name_hash.each do |item, price|
-    if smallest_value == nil
+    if smallest_value == nil || price < smallest_value
       smallest_key = item
       smallest_value = price
-    else
-      if price < smallest_value
-        smallest_key = item
-        smallest_value = price
-      end
     end
   end
   smallest_key
