2c092724b649d7ab93907b48ac0945e344314818
William Barela <william.barela.dev@gmail.com>
2017-03-24 15:17:36 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  return nil if hash == {}
+  ks = hash.map{|k, v| k}
+  vs = hash.map{|k, v| v}
+  min_index = vs.index(vs.min)
+  ks[min_index]
 end
