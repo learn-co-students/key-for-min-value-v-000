42344fca1be762e2e13b5a3a8a8d800d80dbd13d
natasha kelly <kellynatashav@yahoo.com>
2017-03-29 03:37:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+if name_hash == {}
+  return nil
+else
+arr = []
+arr = name_hash.sort_by{|name, value| value}.first
+arr[0]
 end
+end 
+
+
+#Big O notation: N^2 because it is a double loop
