a6ae5b3ac7dcc8be8c4ce50e92a41a2745232f4d
lucketta <lucketta@umich.edu>
2017-04-17 23:36:52 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_so_far = 10000000
+  a_key = ""
+  if name_hash.empty?
+    return nil
+  end
+  name_hash.each do |key, value|
+    if value < min_so_far
+      min_so_far = value
+      a_key = key
+    end
+  end
+  a_key
 end
