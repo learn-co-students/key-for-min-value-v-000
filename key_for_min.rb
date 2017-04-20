a588b4f774d359cd0a617375526f703c8a2a7016
Heather You <heatherhyou@gmail.com>
2017-04-20 14:43:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,5 +16,13 @@
 #end
 
 def key_for_min_value(name_hash)
-  
+  lowest_key = nil #initialize
+  lowest_value = nil #initialize
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
