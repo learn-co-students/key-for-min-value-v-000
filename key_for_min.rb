d538490e62bb3c7f3291f4c7c29c57cf3ff8054e
zoebisch <zoebisch@gmail.com>
2017-05-16 14:13:19 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   temp = 0
-  temp_name = {"name" => nil}
+  temp_name = {name: nil}
   name_hash.each do |pair|
-    if temp_name["name"] == nil || pair[1] < temp
+    if temp_name[:name] == nil || pair[1] < temp
       temp = pair[1]
-      temp_name["name"] = pair[0]
+      temp_name[:name] = pair[0]
     end
   end
-  return temp_name["name"]
+  return temp_name[:name]
 end
