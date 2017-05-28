670fc474504dc918b15406b662fcc7d9eecf7e1d
Luisa Scavo <luisascavo@gmail.com>
2017-05-28 20:21:40 +0000

    refactor with one iterator

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_val = 1000
+  min_val = nil
+  min_key = nil
   name_hash.each do |key, value|
+    min_val = value if min_val.nil?
+    min_key = key if min_key.nil?
+
     min_val = value if min_val > value
+    min_key = key if min_val == value
   end
-  min_val_key = name_hash.find do |key, value|
-    key if value == min_val
-  end
-  if min_val_key == nil
-    nil
-  else
-    min_val_key[0]
-  end
+  min_key
 end
