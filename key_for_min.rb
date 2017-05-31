ed50848d68a84110223f0c2e6f1dbf5c4968c717
Nicholas D'Amico <nickalan82@icloud.com>
2017-05-31 01:05:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if lowest_value == 0
         lowest_key = key
-        lowest_value == value
+        lowest_value = value
       elsif value < lowest_value
         lowest_key = key
         lowest_value = value
