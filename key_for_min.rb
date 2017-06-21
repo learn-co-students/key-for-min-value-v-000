781bda3af9a00edd60d1fbd8111acb3388f63e53
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-21 23:43:14 +0000

    Practice | Refactoring

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,20 +1,13 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-def key_for_min_value(name_hash)
-  lowest_key = ""
-  lowest_value = 0
-  if name_hash == {}
-   return nil
-  else
-    name_hash.each do |key, value|
-      if lowest_value == 0
-        lowest_key = key
-        lowest_value = value
-      elsif value < lowest_value
-        lowest_key = key
-        lowest_value = value
-      end # if inside .each loop
-    end # end of .each loop
-  end # end of if / statement
+require "pry"
+def key_for_min_value(hash)
+  nil if hash.empty?
+
+  lowest_key = nil
+  hash.each do |key, value|
+    lowest_key = key if lowest_key == nil
+    if hash[lowest_key] > value
+      lowest_key = key
+    end
+  end
   lowest_key
 end
