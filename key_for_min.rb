fd966a57fdacd4ead27d7fee6dcbc296763dc9a7
Richard Steinbach <steinbach.dick@gmail.com>
2017-07-06 23:10:09 -0700

    return key for smallest value in hash

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+	return nil if name_hash.empty?
+	v_arr = []
+	k_arr = []
 
+	name_hash.each do |k, v| v_arr << v end
+	name_hash.collect do |k, v| k_arr << k end
+
+		min = v_arr[0]
+		min_k = k_arr[0]
+
+		if v_arr[1] < min
+			min = v_arr[1]
+			min_k = k_arr[1]
+		end
+		if v_arr[2] < min
+			min = v_arr[2]
+			min_k = k_arr[2]
+		end	
+	return min_k
 end
