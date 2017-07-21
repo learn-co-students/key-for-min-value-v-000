fa3e427ea57e7791ae9c6823589df8e477b4e2fe
Alec Alba <alecgalba@gmail.com>
2017-07-21 15:47:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 #\\\\\\ALTERNATIVE SOLUTION FROM GITHUB FORUM//////
 #def key_for_min_value(name_hash)
-  #low_key = nil #set low_key to nil in order for it to return nil when called and therefore not show up
-  #low_value = Float::INFINITY #saw this method online examples, not sure exactly how it works yet
-                              #I think it is to create the highest number possible to compare to the lowest value in the hash
-                              #Or something like that, will do more research
-  #name_hash.each do |key, value| #iterate over the name hash looking for block of code below
+  #low_key = nil ###set low_key to nil in order for it to return nil when called and therefore not show up
+  #low_value = Float::INFINITY ###saw this method online examples, not sure exactly how it works yet
+                              ###I think it is to create the highest number possible to compare to the lowest value in the hash
+                              ###Or something like that, will do more research
+  #name_hash.each do |key, value| ###iterate over the name hash looking for block of code below
     #if value < low_value
       #low_key = key
       #low_value = value
