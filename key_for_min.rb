81649bcecbbcced5d9c51660d1324c8dd4de530d
Donna <github email address>
2017-07-28 11:45:18 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 	new_min_value = nil
 	new_min_key = nil
 	name_hash.each do |key, value|
-		if new_min_value == nil || value < new_min_value 
+		if value < new_min_value || new_min_value == nil 
 			new_min_value = value
 			new_min_key = key 
 		end
