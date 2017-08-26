64ed2bf608d8b5545e3f6b2e1bafbafc670ef81f
Sergio Correa <correa.sergio@gmail.com>
2017-08-26 00:41:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,3 +19,16 @@ def key_for_min_value(name_hash)
     nil
   end
 end
+
+#### ALTERNATIVE ####
+def key_for_min_value(name_hash)
+  min_key = nil
+  min_value = nil
+  name_hash.each do |key, value|
+    if min_value == nil || value < min_value
+      min_value = value
+      min_key = key
+    end
+  end
+  min_key
+end
