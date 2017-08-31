bce04cfaa6a95d79233bfcd18dea9ad3084724f7
Bret Burau <burhooo@gmail.com>
2017-08-31 10:35:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
+  smallest = 100000 #define state?
+  winner = ""
+  if name_hash.empty? # = {} #test if empty
+    winner = nil
+  else
+    name_hash.each do |name, points| #find smallest value
+      #binding.pry
+      if points < smallest
+        smallest = points
+        winner = name
+      end
+    end
+  end
+  winner
 end
