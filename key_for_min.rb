db62400a2d42835081bbdbbe41167c26129e3844
matos89 <matos89@gmail.com>
2017-09-03 12:01:31 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  low_key = []
-  low_value = []
+  low_key = nil
+  low_value = nil
     name_hash.each do |object, price|
       if low_value[0] == nil || price < low_value[0].to_i
-          low_value.replace([price])
-          low_key.replace([object])
+          low_value = price
+          low_key = object
       end
     end
     if name_hash.empty?
       nil
     else
-      return low_key[0]
+      return low_key
     end
   end
