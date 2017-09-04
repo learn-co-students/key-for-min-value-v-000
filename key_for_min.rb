8c3e35b37ffb55ed8377bf6938e0753e1d319150
Lara Sanders <larasanders@gmail.com>
2017-09-04 19:47:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
+name_hash = {}
 def key_for_min_value(name_hash)
-
+  youngest = ""
+  if name_hash == {}
+    puts name_hash
+  else 
+  youngest = name_hash.sort_by {|name, age| age}.first
+    youngest[0]
+  
+end
 end
