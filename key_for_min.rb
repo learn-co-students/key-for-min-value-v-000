3a87ea3e49a53903eeb666536975ed4e7d1048b7
Christine <christinetran825@gmail.com>
2017-09-05 02:40:02 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = name_hash.inject do |key, value| #inject on a hash converts the hash into an array before being passed through. Inject "captures" the value. The key is key.first, and value is value.last.
+  lowest_key = name_hash.reduce do |key, value| #inject on a hash converts the hash into an array before being passed through. Inject "captures" the value. The key is value.first, and value is value.last.
     key.last > value.last ? value : key
   end
     if name_hash == {}
