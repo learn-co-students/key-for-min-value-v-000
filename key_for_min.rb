fed3c307a2a15375e97a483426be7d3b1bbabcfa
Sergio Correa <correa.sergio@gmail.com>
2017-09-06 01:17:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -22,6 +22,16 @@ end
 
 #### ALTERNATIVE ####
 def key_for_min_value(name_hash)
+  values = name_hash.collect {|key, value| value }
+  min_value = values.sort[0]
+  if !name_hash.empty?
+    name_hash.each { |key, value|return key if value == min_value }
+  else
+    nil
+  end
+end
+#### ALTERNATIVE ####
+def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
