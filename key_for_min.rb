32177c306acb62748c62c460091d86d3cc5988a5
smithwebtek <brad@smithwebtek.com>
2017-09-11 23:08:29 -0400

    clean up indenting

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,16 @@
-
+require 'pry'
 def key_for_min_value(asdf)
   values = []
   lv = 0  #---low value
   hv = 0  #---high value
-    if asdf.empty? 
-      return nil 
-      end
-    asdf.map {|k,v| values << v}
-    until values.length == 1 do 
-      hv = values.max
-      values.delete(hv)
-    end
-    lv = values[0]
-    asdf.key(lv)
-  end
+	if asdf.empty? 
+		return nil 
+	end
+	asdf.map {|k,v| values << v}
+	until values.length == 1 do 
+		hv = values.max
+		values.delete(hv)
+	end
+	lv = values[0] 
+	asdf.key(lv)
+end
