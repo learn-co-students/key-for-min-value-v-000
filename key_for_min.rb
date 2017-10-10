7b3437b0babdc0734d8c7c03446b29d106f6f3cb
Frank McCallion <keith@sensortech.com>
2017-10-10 12:53:00 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = 10000
+  min_value_key = nil
+
+  name_hash.each do |name, age|
+
+    if age < min_value
+      min_value_key = name
+      min_value = age
+    end
+
+  end
+
+  min_value_key
+
 end
