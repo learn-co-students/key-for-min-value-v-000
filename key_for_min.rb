9dfdcd01d743fb9af1b7be4fa7162a71091267d1
Trisha Garrett <trisha.garrett@gmail.com>
2017-10-29 21:10:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,16 +3,17 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
-  min_key = 1
+  min_key = nil
   name_hash.each do |key, value|
-    if value < min_value
+    if min_value == nil || value < min_value
       min_value = value
        min_key = key
 
     end
   end
-
+min_key
 end
+
 
 
 
