afed03127a3e54070d2051237923fe60b40f44ad
Melissa Rudick <30333925+melrudick@users.noreply.github.com>
2017-11-02 20:52:58 -0500

    refactor

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = nil # Ashley had me use a large number, and Corinne recommended using nil. I can't get nil to work without refactoring the code.
-  lowest_key = nil #start with a high number that will be replaced
+  lowest = nil 
+  lowest_key = nil 
   name_hash.each do |name, value|
-    if lowest_key == nil || value < lowest #if the current value is less than the current lowest number then it must in fact be the lowest number
-      lowest = value #make sure to replace the lowest number with the new lowest number
-      lowest_key = name #set the key of that lowest number
+    if lowest_key == nil || value < lowest 
+      lowest = value 
+      lowest_key = name 
     end
   end
   lowest_key #return lowest key
 end
+
+# had lowest_key set at 100000, which isn't a safeguard to failure. to use nil, had to use nil OR passed value as lowest
