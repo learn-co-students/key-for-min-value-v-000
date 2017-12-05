1fd87fc9a952b7a42dff52fbd15ca8198ff0ca9e
Brittany Morris <brittmmendez@gmail.com>
2017-12-05 13:38:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# note to self, you can do the following code too:
+#def sorting(ikea)
+#ikea.min_by{|item,price| price}[0]
+#end
 
+def key_for_min_value(name_hash)
+      smallest_value=nil
+      smallest_key=nil
+
+    name_hash.collect do|key,value|
+      if smallest_value==nil
+      smallest_value=value
+      smallest_key=key
+    elsif value<smallest_value
+      smallest_value=value
+      smallest_key= key
+    end
+
+  end
+  return smallest_key
 end
