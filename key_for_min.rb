90808a863b248414a52b634eacd971c22b05a595
jtorre20 <jtorres1989@gmail.com>
2017-12-12 17:05:06 -0500

    initial commit

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
+  return nil if name_hash.empty?
+    
+  min = name_hash.first[1]
+  low_key = name_hash.first[0]
+  name_hash.each do |key, val|
+    if val < min
+        min = val
+        low_key = key
+            
+    end
 
+  end
+    low_key
 end
