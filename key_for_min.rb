6aa765249e0af0bbd59d59b201599774f78237af
jtorre20 <jtorres1989@gmail.com>
2017-12-13 09:21:48 -0500

    added guard clause and next

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  return nil if name_hash.empty?
-  binding.pry  
+  return nil if name_hash.empty?  
   min = name_hash.first[1]
   low_key = name_hash.first[0]
   name_hash.each do |key, val|
-    if val < min
-        min = val
-        low_key = key
-            
-    end
-
+    next if val > min # alternate way  if val < min
+      min = val
+      low_key = key            
+      #binding.pry
+    #end
   end
     low_key
 end
