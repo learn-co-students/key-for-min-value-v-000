bd5df76c96bfe6cb5579414d6ea62a75b6a4124e
Eddie Melo <heriberto@melo.nyc>
2017-12-13 12:39:52 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,22 @@
 
 def key_for_min_value(name_hash)
   #name_hash.sort_by {|name, volume| volume}.first}
-  if name_hash.empty?
-    return nil
+  #if name_hash.empty?
+  #  return nil
+  #end
+  #new_h = name_hash.sort_by {|name, volume| volume}
+  #new_h.each do |name, volume|
+  #    return name
+  #end
+
+  lowest_name = nil
+  lowest_volume = nil
+
+  name_hash.each do |name, volume|
+    if lowest_name == nil || volume < lowest_volume
+      lowest_volume = volume
+      lowest_name = name
+    end
   end
-  new_h = name_hash.sort_by {|name, volume| volume}
-  new_h.each do |name, volume|
-      return name
-  end
+  lowest_name
 end
