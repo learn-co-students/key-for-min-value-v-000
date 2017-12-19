6a46252f7127e27eda747b0051c6ef483d0108fd
funemployeddad <funemployeddad@gmail.com>
2017-12-19 00:26:10 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-#key with the smallest value would say to put the values into an array and use .sort -> then you can return the key that corresponds to that value. what if there were two keys that had the same value, could it tell the difference? ex:    :boys = 25, :girls = 25. if we sorted the '25's' could it tell which key that went to (boys v girls?)
-
-#pseudocode below
-name_hash.collect do |name, value|
-   if value[0] == value
-     return :name
+#ensure a return of nil if passed an empty has by making variables for the key and value pair and setting them to nil by default. This will only change if we are passed a valid hash.
+  lowest_key = nil
+  lowest_value = nil
+    name_hash.each do |key, value|   #iterate over the key/value pairs
+      if lowest_value == nil || value < lowest_value
+        lowest_value = value
+        lowest_key = key
+        #if the lowest_value variable is still at nil, or if it is greater than the current value being yielded, set that variable to the current pass through the block for both value and key
+      end
     end
-   end
-end
+    lowest_key    #call the method so it gets a value stored
+  end
