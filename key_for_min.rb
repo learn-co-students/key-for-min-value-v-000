39ad564a0a79e4a101e8dd836e28cf7c214ee1db
Nathan Dietz <nsdietz@gmail.com>
2017-12-20 12:01:08 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash != {}
+    new_array = name_hash.sort_by {|name, age| age}
+    new_array[0][0]
+  else nil
+  end
 end
