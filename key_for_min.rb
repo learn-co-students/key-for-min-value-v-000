c2812570f11209f87dea1e8eedf2e3f4f6e97f67
kostanzhoglo <jeffreywithers@gmail.com>
2017-12-21 02:26:42 -0500

    Add min value method

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_val = nil
+  hash.each do |key, value|
+    if lowest_val == nil || value < lowest_val
+      lowest_val = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
