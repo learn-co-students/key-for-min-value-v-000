726ca905e22c04d36a9ee4c235699a15bbdb37f1
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-21 18:56:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,17 +8,40 @@
 #return the variable outside of the hash
 
 
+## original solution
+# is faulty becuase I have to initialize lowest_value that might make code
+#break if value is greater than 1000000.
+#Avi's solution sets default lowest value to nil and incorporates that into
+#if condition
+
+=begin
+MY ORIGINAL SOLUTION
 def key_for_min_value(name_hash)
-  smallest = 100000
-  smallest_name = " "
+  lowest_value = 1000000
+  lowest_key = nil
     if name_hash.empty?
       return nil
     end
-    name_hash.each do |name, value|
-      if value < smallest
-        smallest = value
-        smallest_name = name
+    name_hash.each do |key, value|
+      if value < lowest_value
+        lowest_value = value
+        lowest_key = key
       end
     end
-  smallest_name
+  lowest_key
+end
+
+
+#their SOLUTION
+=end
+def key_for_min_value(name_hash)
+  lowest_value = nil
+  lowest_key = nil
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
