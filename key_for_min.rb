9095724124dede41e781eff9e5ebadc6694a65df
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-05 05:33:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,17 @@
 
 def key_for_min_value(name_hash)
   min_value = nil
-  current_key_value= nil
+  min_key = nil
    name_hash.each do |key, value|
      if min_value == nil
         min_value = value
-       current_key_value = key
+       min_key = key
   else
      if min_value > value
         min_value = value
-        current_key_value = key
+        min_key = key
       end
     end
   end
-  current_key_value
+  min_key
 end
