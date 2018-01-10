b97ef143f662e5022f4120bf44f67555613396e4
Allyson Wesman <allysonwesman@gmail.com>
2018-01-10 15:15:32 -0800

    solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+  keys = name_hash.collect { |key, value| key }
+  values = name_hash.collect { |key, value| value }
+
+  current_min = values[0]
+  min_index = 0
+  i  = 0
+
+  values.each do |value|
+    if value < current_min
+      current_min = value
+      min_index = i
+    end
+    i += 1
+  end
+
+  return keys[min_index]
 end
