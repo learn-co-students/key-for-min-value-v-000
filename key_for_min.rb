79a6a41536cb6a599a8f04dbfbadd0bce50fcdb1
jacobsilver2 <jacobsilver2@mac.com>
2018-01-12 10:58:04 -0500

    did the work. a little sloppy

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
+require "pry"
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   if name_hash.length == 0 
+      return nil
+   end
+   
+   valArr = name_hash.collect do |key, value|
+      value
+   end
+   sorted = valArr.sort
+   name_hash.collect do |key, value|
+      if value == sorted[0]
+         return key
+      end
+   end
 end
