4ea16a354984c3f1a913366fc36eda0552e3cbc8
jljardon <github email address>
2018-01-14 21:10:46 -0500

    add #key_for_min_value with solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+   low_val = (2**(0.size * 8 - 2) - 1) # largest posible fixnum
+   low_key = nil
 
+   name_hash.each do |key, value|
+      value.nil? ? break : (low_val > value ? (low_val = value; low_key = key) : nil)
+   end
+   low_key
 end
