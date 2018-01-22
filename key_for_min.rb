a1307650aeb2e0abababf3d69e083acd894b4691
Jayon Kim <jpkim921@gmail.com>
2018-01-22 00:52:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.length == 0
   
+<<<<<<< HEAD
   name = []
   value = []
   
@@ -23,5 +24,20 @@ def key_for_min_value(name_hash)
  
   return name[min_value_idx]
 
+=======
+  min_key = ""
+  min_value = ""
+>>>>>>> 0d280143e0a50113e901337f10dbc7b73bd7e6d5
 
+    name_hash.each do |key, value|
+    min_value = value
+    min_key = key
+    
+    if value < min_value
+      min_value = value
+      p min_key = key
+    end
+    
+  end
+  min_key
 end
