27a0afaa64175427980bef48360be791e9cb3e6b
Chris Henry <cdhenry@gmail.com>
2018-02-06 23:32:55 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,13 @@
-require 'pry'
-
 def key_for_min_value(name_hash)
-  key_array = []
-  value_array = []
-  key_array = name_hash.collect {|key, value| key}
-  value_array = name_hash.collect {|key, value| value}
+  lowest_key = nil
+  lowest_value = nil
 
-  index = 0
-  key_index = 0
-  lowest_val = value_array[0]
-  while index < value_array.length
-    if value_array[index] < lowest_val
-      lowest_val = value_array[index]
-      key_index = index
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
-    index += 1
   end
 
-  return key_array[key_index]
+  lowest_key
 end
