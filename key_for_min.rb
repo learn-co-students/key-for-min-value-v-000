86a59a8adf0315fadb9f578a5c09678457bbc135
Zac Willmington <zac.willmington@gmail.com>
2018-02-09 04:49:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,34 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# def key_for_min_value(hash)
+# 	vals_arr = []
+#   adzzie =[]
+# 	hash.each do |k , v|
+# 		vals_arr<< v
+# 	end
+#
+# 	low = vals_arr.reduce{ |sum, num| sum > num ? num : sum}
+#
+#   hash.collect do |key, value|
+#       value == low
+#     adzzie << "#{key}"
+#     end
+# adzzie
+#end
+def key_for_min_value(hash)
+	vals_arr = []
+
+	hash.each do |k , v|
+		vals_arr<< v
+	end
+
+	low = vals_arr.reduce{ |sum, num| sum > num ? num : sum}
+
+  # hash.select do |key, value|
+       hash.key(low)
+
+
+  
 
 end
