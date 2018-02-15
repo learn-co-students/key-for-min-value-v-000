94584aaf54c263588a4b3218aad748a9fa358574
Joseph Vint <josephvint@me.com>
2018-02-15 23:33:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,3 +13,27 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
+
+def key_for_min_value_prototype(name_hash)
+  hash_array = Array.new
+  hash_key = ""
+  index = 0
+
+  if name_hash == {}
+    return nil
+  else
+    values = name_hash.each do |key,value|
+      hash_array[index] = value
+
+      if value <= hash_array[0]
+        hash_array[0] = value
+        hash_key = key
+        index += 1
+      else
+        index += 1
+      end
+
+    end
+  end
+  hash_key
+end
