714e1966d084275a2de16a4a6407c5984a825665
Holden Altaffer <haltaffer@gmail.com>
2018-02-24 10:34:47 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+result = name_hash[:blake]
+name_hash.collect do |key, value|
+  if name_hash[key] < result
+    result = name_hash[key]
 
+  end
+end
+  name_hash.key(result)
 end
