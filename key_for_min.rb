c551802fa05adb59176ccba5ca69a079b79701b0
Victoria Meng <nights1ststar@protonmail.com>
2018-03-10 22:01:44 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash.empty?
+  values = []
+  name_hash.each { |key, value| values << value }
+  min_value = values.inject { |memo, value| memo < value ? memo : value }
+  name_hash.rassoc(min_value).first
 end
