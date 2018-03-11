c1ceaf1a172e33da3b5dbe9dcc252d4970ad8782
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-11 20:08:06 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
+  sorted_h = name_hash.sort_by { |key, val| val}.to_h
+  if sorted_h == {}
+    return nil
+  else
+     sorted_h.each do |k, v|
+       return k
+     end
+  end
+
 
 end
