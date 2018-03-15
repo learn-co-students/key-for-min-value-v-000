3345bb51c534a265957f3c1a5d1662f3de1e3350
David Markel <davidgmarkel@gmail.com>
2018-03-15 17:27:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,13 @@
 def key_for_min_value(name_hash)
-  name_hash_array = []
-    name_hash.each do |a, b|
-      name_hash_array.push(a, b)
-    end
+  min_key = nil
+  min_value = nil
 
-  while name_hash_array.length > 2
-    if name_hash_array[1] > name_hash_array[3]
-      name_hash_array.delete_at(0)
-      name_hash_array.delete_at(0)
-    else
-      name_hash_array.delete_at(2)
-      name_hash_array.delete_at(2)
+  name_hash.collect do |name, value|
+    if min_value == nil || value < min_value
+      min_value = value
+      min_key = name
     end
   end
 
-  name_hash_array[0]
+  min_key
 end
