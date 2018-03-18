d14a41f72c3842ca5ae0990fc89876db34699ec8
Karen G <kgraves0701@gmail.com>
2018-03-18 02:00:25 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   holder = 1000000
+  hold_name = ""
   name_hash.each do |name, value|
      if value < holder
-     return name
+     hold_name = name
      holder = value
-     else
-     return nil
      end
    end
+   return hold_name
 end
