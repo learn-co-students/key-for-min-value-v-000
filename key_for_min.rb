901383e44c3fef59a90c861eb9f42250f61d1f5d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-28 23:56:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 999999999999;
+  rtn = nil
+  name_hash.each do | key, val |
+    if val<min
+      min = val
+      rtn = key
+    end
+  end
+  rtn
 end
