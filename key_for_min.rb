7bbf41ae2e493f7bcc7d300ab0c33f8aed09a22c
Joanne Lee <vernapii@gmail.com>
2018-04-04 15:05:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,13 @@
-desserts = {
-  :brownies => 10,
-  :challah_bread => 20,
-  :babka => 15,
-  :cookies => 30,
-  :croissants => 40,
-  :coffee => 40,
-  :heavy_cream => 10,
-  :sugar_packets => 100,
-  :monkey_bread => 8
-}
+
 def key_for_min_value(hash)
   smallest_value = hash.first[1]
   smallest_key = nil
 hash.each do |key, value|
-  if value < smallest_value
+  if smallest_value == value || value < smallest_value
    smallest_value = value
    smallest_key = key
+ else hash == nil || hash == {}
+  return  nil
 end
 end
 smallest_key
