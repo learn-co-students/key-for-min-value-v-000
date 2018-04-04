db52304edf758ef2312b98253a38489cd1ac76c1
Joanne Lee <vernapii@gmail.com>
2018-04-04 15:07:05 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,11 @@
-
 def key_for_min_value(hash)
   smallest_value = hash.first[1]
   smallest_key = nil
-hash.each do |key, value|
-  if smallest_value == value || value < smallest_value
-   smallest_value = value
-   smallest_key = key
-
-end
-end
-smallest_key
+  hash.each do |key, value|
+   if value < smallest_value
+    smallest_value = value
+    smallest_key = key
+   end
+  end
+  smallest_key
 end
