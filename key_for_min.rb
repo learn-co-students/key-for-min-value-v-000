998e15556d74197e465963a4141d04bf2bc70c1d
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-09 01:54:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_value = nil
-  smallest_key = ""
+  smallest_key = nil
   name_hash.each do |key, value|
    if smallest_value == nil || value < smallest_value
      smallest_value = value
