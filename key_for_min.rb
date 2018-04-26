b4acdf16fb5d0bb65b8a0d69e288cb187a69d37c
Suchitra Naik <susanaik@gmail.com>
2018-04-26 18:26:54 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,14 @@
 
 def key_for_min_value(name_hash)
   
-  if(name_hash==[])
+  if(name_hash.empty?)
     return nil 
   end
   
-  winner_key = ""
+  
   # sort hash according to value, lowest comes first
   sorted_name_hash = Hash.new 
   sorted_name_hash = name_hash.sort_by { |key_name, value_name| value_name}
   
-  sorted_name_hash.first.each do |key, val|
-    winner_key = key
-  end
-  return winner_key
+  return sorted_name_hash[0][0]
 end
