dd220fcb3fa07816c07815f1ef84ade7dcc8e301
Christiana Vandermale <christianavdm@gmail.com>
2018-05-04 01:15:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   smallest_key = ""
-  smallest_value = 1000000000000000
+  smallest_value = nil
   if name_hash == {}
     return nil
   else 
     name_hash.each do |key, value|
-      if value < smallest_value
+      if smallest_value == nil || value < smallest_value
         smallest_value = value
         smallest_key = key
       end
