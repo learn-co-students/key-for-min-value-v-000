5a76693e2406629cb6f761446a1f6ec46393a6e7
Kenlyn Terai <kenlyn@flatironschool.com>
2018-05-16 15:19:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,8 @@
 
 def key_for_min_value(name_hash)
   smallest_key = nil
-  smallest_value = nil
-  name_hash.each do |name, value|
-    if smallest_value == nil || value < smallest_value
+  smallest_value = 100
+  if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_key = name
     end
