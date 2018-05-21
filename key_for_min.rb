2614133c5f248ae0ad34cde4f4fc746eaa315f97
Josh Stegmeier <josh@steg.me>
2018-05-21 23:19:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# require 'pry'
 
+def key_for_min_value(name_hash)
+  bestVal = false
+  bestName = false
+
+  if name_hash.empty? == true
+    return nil
+  else
+    name_hash.collect do |name, value|
+      if bestVal == false
+        bestVal = value
+        bestName = name
+      elsif value < bestVal
+        bestVal = value
+        bestName = name
+      end
+    end
+    bestName
+  end
 end
