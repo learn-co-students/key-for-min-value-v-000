096ac02e17775a5c7f7c759926c4cf2d8a81c229
Taylor Williams <taylor@thewilliams.net>
2018-05-21 00:21:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if !name_hash.empty?
+    min_key = ""
+    min_age = 400000
+    name_hash.each do |kid, age|
+      if age < min_age
+        min_age = age
+        min_key = kid
+      end
+    end
+    min_key
+  end
 end
