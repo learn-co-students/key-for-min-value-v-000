6bc865fbe59912e3df53f352814146a6ccaa7c66
Justine Bricknell <justinegayton@gmail.com>
2018-06-27 21:51:43 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,32 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-#collect
-#|key, value|
-#key
-
+#collect. return only the key!
 #empty hash = NIL
-
-#smallest value SORT last?
-
-  #name_hash.flatten do |key, value|
-
-
-#  value.flatten.
-
-#  keep_if { |key, value|
+#smallest value SORT last? How to sort w/o using sort. need to iterate and compare each time!
+# would these methods work?? flatten (change into an array but then would need to change back into a has?)
+#  keep_if
 
 def key_for_min_value(name_hash)
-  index = 0
-  lowest_hash = {}
+  small_value = nil
+  small_values_key = nil
 
-  name_hash.collect do |key, value|
-    if value[index] < value[index + 1]
-      lowest = value
-      index = index + 1
-      key_for_min_value[value]
-
-      return lowest
-    end
+  name_hash.each do |key, value|
+  if small_value == nil || value < small_value
+    small_value = value
+    small_values_key = key
   end
-end 
+end
+    small_values_key
+end
