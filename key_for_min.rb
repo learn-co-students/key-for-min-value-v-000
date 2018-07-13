9a7886c2b0224fd6cc57edb3ba0d0b779daa9fa1
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-13 18:11:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,11 @@
 def key_for_min_value(name_hash)
   lowest =  nil
   lowest_key = nil
-  # name_hash.each do |n| :key[value] < :key[value]
   name_hash.each do |k, v|
   if lowest.nil? || v < lowest
     lowest = v
     lowest_key = k
     end
-    # return name_hash.key(lowest)
-    # This needs to be the ke
 end
     return lowest_key
 
