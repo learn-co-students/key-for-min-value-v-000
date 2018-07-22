56eaa8fe9e7a501666de1ad7fcf5171be34786ed
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-22 22:50:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,11 @@
-
-
-
-def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  else
-  min_value = name_hash.first[1]
-  min_key = name_hash.first[0]
+ def key_for_min_value(name_hash)
+  min_key = nil
+  min_value = nil
   name_hash.each do |key, value|
-    if value < min_value
-    min_value = value
-    min_key = key
-    end
+  if min_value == nil || value < min_value
+  min_value = value
+  min_key = key
   end
- min_key
- end
- end
+end
+  min_key
+end
