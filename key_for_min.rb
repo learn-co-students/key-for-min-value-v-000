6917f723179bcbff05b1649ef3ee14a8f0ea3c90
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-25 03:02:48 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,30 +1,27 @@
 def key_for_min_value(name_hash)
+  # Convert hash to array
   name_a = name_hash.to_a
 
+  # Default key value
   d_value= 1000
-  d_key= nil
+  d_key= 0
 
+  # Iterate new array 
   name_a.each do |i|
       # If current value is lower than default, change value&key 
       if i[1] < d_value
         d_value = i[1]
         d_key = i[0]
       end
-  end
-return d_key
+
+   end
+
+  return d_key
+
 end  
+
+
+
   
- # ITERATE THROUGH THE name_hash 
-  # CREATE LOCAL VARIABLE TO HOLD CURRENT NUMBER 
-    # push current number to local VARIABLE
-  # GO TO THE NEXT NUMBER IN THE ITERATION
-    # this number < number in local variable? 
-      # yes? push this number to local variable
-      # no? ignore it 
-  # FINISH ITERATION 
-  # CLOSE THE ITERATIVE LOOP (end)
-  # RETURN THE VARIABLE 
-  # CLOSE THE METHOD (end)
-  # <<
-  
+
   
