89eda71b0e530584aa2143c403e8937ac1185b13
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-03 22:51:55 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,22 @@
 
 def key_for_min_value(name_hash)
 
+  temp_array = Array.new
+  i = 0
+  if name_hash.empty?
+    return nil
+  else
+    name_hash.each do |key, value|
+      temp_array[i] = value
+      i += 1
+    end
+    temp_array = temp_array.sort
+    smallest_value = temp_array[0]
+
+    name_hash.each do |key, value|
+      if value == smallest_value
+        return key
+      end
+    end
+  end
 end
