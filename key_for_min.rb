adc481235a24ba7d63c505f894ad5d37a70181e2
Ashley Wong <ashwong04@gmail.com>
2018-09-05 15:08:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@
 
 def key_for_min_value(name_hash)
     lowest_value = nil
+    lowest_key = nil
+      
     name_hash.each do |key, value|
-      if value < 
-        lowest_value = key
-    
+      if value < lowest_value || lowest_value == nil
+        lowest_value = value
+        lowest_key = key
       end
     end
-  return lowest_value
+  return lowest_key
 end
