4942d6ee136fff8e73b85fdefc25f14f7cbbf143
Sherry Martin <sm674@cornell.edu>
2018-09-17 22:03:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,3 +8,16 @@ def key_for_min_value(name_hash)
     p name_hash.reduce{ |f, s| f.last > s.last ? s : f }.first
 end
 end
+
+#Alternate solution
+# def key_for_min_value(hash)
+#   lowest_key = nil
+#   lowest_value = nil
+#   hash.each do |k, v|
+#     if lowest_value == nil || v < lowest_value
+#       lowest_value = v
+#       lowest_key = k
+#     end
+#   end
+#   lowest_key
+end
