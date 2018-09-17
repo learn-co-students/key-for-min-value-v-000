bbb0762af491f82d9014e079ad5fbac9b5be348c
Sherry Martin <sm674@cornell.edu>
2018-09-17 22:01:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,22 +8,3 @@ def key_for_min_value(name_hash)
     p name_hash.reduce{ |f, s| f.last > s.last ? s : f }.first
 end
 end
-  # # Convert hash to array
-  # name_a = name_hash.to_a
-
-  # # Default key value
-  # d_value= 1000
-  # d_key= 0
-
-  # Iterate new array 
-#   name_a.each do |i|
-#       # If current value is lower than default, change value&key 
-#       if i[1] < d_value
-#         d_value = i[1]
-#         d_key = i[0]
-#       end
-#   end
-#   return d_key
-# else
-#   nil
-# end  
