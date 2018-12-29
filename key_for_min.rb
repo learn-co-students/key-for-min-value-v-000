8df30ab37a278aa04d405827c2a5115befbe3652
RaineSkye <38991973+RaineSkye@users.noreply.github.com>
2018-12-29 15:51:54 -0800

    Update key_for_min.rb

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,3 +12,19 @@ def key_for_min_value(name_hash)
   end
   lowest_key
 end
+
+=begin
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+end
+
+=end
