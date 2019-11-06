11de5ec81c2cfa5852afe7838c63f0b91e915f52
Neil Ryan <neilmarkoliverryan@gmail.com>
2019-11-05 21:05:06 -0800

    Update #key_for_min_value
    Second run through completed without relying on FLOAT:INFINITY which i don't think I understood at the time. And maybe not yet.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,19 +1,29 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-  	min_value = Float::INFINITY
-  	name_hash.each do |key, value|
-  		if name_hash[key] < min_value
-  			min_value = name_hash[key]
-  		end
-  	end
-  	name_hash.each do |key, value|
-  		if name_hash[key] == min_value
-  			return key
-  		end
-  	end
+    cost_array = name_hash.collect {|name, cost| cost}
+    lowest_value = nil
+
+    cost_array.each do |cost|
+      temp = cost_array[0]
+      if cost < temp
+        lowest_value = cost
+      else
+        lowest_value = temp
+      end
+    end
+
+    name_hash.each do |name, cost|
+      if cost == lowest_value
+        return name
+      end
+    end
+
   end
+
 end
+
