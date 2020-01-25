cf1baa345c73e418c4a14f3c81e20e497ac8222a
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-25 08:58:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+name_hash = {
+  "shape" => 20,
+  "drinks" => 6,
+  "plants" => 17
+  }
 def key_for_min_value(name_hash)
-
+  if name_hash = {}
+    nil
+    name_hash.sort do |key, value|
+    key <=> value
+  end.first[0]
 end
